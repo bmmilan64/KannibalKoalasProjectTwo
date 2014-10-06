098415ef61c7edbf87c304af4d9763523664919d
Brooke Milan <bmm3443@rit.edu>
2014-10-06 09:22:45 -0400

    Base Project
    rudimentary movement, smooth follow camera and a space

---
+++ Project2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+
+	void OnDestroy()
+	{
+		// Game Over.
+		// Add the script to the parent because the current game
+		// object is likely going to be destroyed immediately.
+		transform.parent.gameObject.AddComponent<GameOverScript>();
+	}
+}
---
+++ Project2/Assets/Scripts/ShotScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour 
+{
+	/// Damage inflicted
+	public int damage = 1;
+
+	/// Projectile damage player or enemies?
+	public bool isEnemyShot = false;
+	
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		Destroy(gameObject, 20); // 20sec
+	}
+}
